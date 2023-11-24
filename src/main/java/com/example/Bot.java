dc03b2d78fc35dc6d34361adee42bdc7d1f9d952
Johannes (Jotrorox) <jotrorox@gmail.com>
2023-11-24 23:35:50 +0100

    first commit

---
+++ src/main/java/com/example/Bot.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+public class Bot {
+    public static void main( String[] args ) {
+        JDABuilder builder = JDABuilder.createDefault("BOT_TOKEN");
+
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+        builder.setBulkDeleteSplittingEnabled(false);
+        builder.setActivity(Activity.playing("with JDA!"));
+        builder.build();
+    }
+}
